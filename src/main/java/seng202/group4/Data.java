ab752879df41dc473afef73476669c487fc50b9b
Joseph Gardner <jjg64@uclive.ac.nz>
2016-08-12 18:14:13 +1200

    Made the skeleton for the model

---
+++ src/main/java/seng202/group4/Data.java
@@ -0,0 +1,19 @@
+package seng202.group4;
+
+/**
+ * Created by jjg64 on 12/08/16.
+ */
+class Flight {
+}
+
+class Airport {
+
+}
+
+class Airline {
+
+}
+
+class Route {
+
+}
---
+++ src/main/java/seng202/group4/Repository.java
@@ -0,0 +1,30 @@
+package seng202.group4;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jjg64 on 12/08/16.
+ */
+public class Repository {
+
+}
+
+class FlightRepository extends Repository {
+    ArrayList<Flight> Flights = new ArrayList<Flight>();
+
+}
+
+class AirportRepository extends Repository {
+    ArrayList<Airport> Flights = new ArrayList<Airport>();
+
+}
+
+class AirlineRepository extends Repository {
+    ArrayList<Airport> Flights = new ArrayList<Airport>();
+
+}
+
+class RouteRepository extends Repository {
+    ArrayList<Airport> Flights = new ArrayList<Airport>();
+
+}
