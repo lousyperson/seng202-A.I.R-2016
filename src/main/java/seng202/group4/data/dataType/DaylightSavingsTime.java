f1fe283b876dd723b99768396d4b542671990682
Joseph Gardner <jjg64@uclive.ac.nz>
2016-08-18 19:37:29 +1200

    Updated Airline class, started working on Airline parser, made DST enum

---
+++ src/main/java/seng202/group4/data/dataType/DaylightSavingsTime.java
@@ -0,0 +1,14 @@
+package seng202.group4.data.dataType;
+
+/**
+ * Created by jjg64 on 18/08/16.
+ */
+public enum DaylightSavingsTime {
+    E,  // Europe
+    A,  // US/Canada
+    S,  // South America
+    O,  // Australia
+    Z,  // New Zealand
+    N,  // None
+    U   // Unknown
+}
--- src/main/java/seng202/group4/data/parser/AirlineParser.java
+++ src/main/java/seng202/group4/data/parser/AirlineParser.java
@@ -1,7 +1,16 @@
 package seng202.group4.data.parser;
 
+import java.io.BufferedReader;
+
 /**
  * Created by jjg64 on 15/08/16.
  */
 public class AirlineParser {
+    private BufferedReader file;
+
+    AirlineParser(BufferedReader file) {
+        this.file = file;
+    }
+
+
 }
