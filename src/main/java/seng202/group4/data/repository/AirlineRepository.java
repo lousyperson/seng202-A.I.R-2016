523368f2c77e70d875a4e0f6c92d2fbc367451b7
Joseph Gardner <jjg64@uclive.ac.nz>
2016-08-18 20:53:53 +1200

    Updated Airline class, started working on Airline parser, made DST enum

--- src/main/java/seng202/group4/data/repository/AirlineRepository.java
+++ src/main/java/seng202/group4/data/repository/AirlineRepository.java
@@ -1,12 +1,10 @@
 package seng202.group4.data.repository;
 
-import seng202.group4.data.dataType.Airport;
-
 import java.util.ArrayList;
 
 /**
  * Created by jjg64 on 15/08/16.
  */
 public class AirlineRepository extends Repository {
-    ArrayList<Airport> Airlines = new ArrayList<Airport>();
+    ArrayList<Airline> Airlines = new ArrayList<Airline>();
 }
--- src/main/java/seng202/group4/data/repository/AirportRepository.java
+++ src/main/java/seng202/group4/data/repository/AirportRepository.java
@@ -1,11 +1,10 @@
 package seng202.group4.data.repository;
 
-import seng202.group4.data.dataType.Airline;
 import java.util.ArrayList;
 
 /**
  * Created by jjg64 on 15/08/16.
  */
 public class AirportRepository extends Repository{
-    ArrayList<Airline> Airports = new ArrayList<Airline>();
+    ArrayList<Airport> Airports = new ArrayList<Airport>();
 }
