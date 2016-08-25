701bc3ea23002f86a00af13e68ceb84fbb7f0d50
Joseph Gardner <jjg64@uclive.ac.nz>
2016-08-25 14:32:21 +1200

    Airline Validator initialised

--- src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
+++ src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
@@ -1,7 +1,36 @@
 package seng202.group4.data.parser.validator;
 
+import seng202.group4.data.dataType.Airline;
+import seng202.group4.data.parser.AirlineParser;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by jjg64 on 25/08/16.
  */
 public class AirlineValidator {
+    private final int MAX_ITEMS_PER_LINE = 8;
+    private String filepath;
+    private BufferedReader file;
+    private String[] splitLine = new String[MAX_ITEMS_PER_LINE];
+    private String splitBy = ",";
+    private int ID;
+    private boolean active;
+    private String currentLine;
+
+    public AirlineValidator(String filepath) throws FileNotFoundException {
+        this.filepath = filepath;
+        this.file = new BufferedReader(new FileReader(filepath));
+    }
+
+    public ArrayList<Airline> makeAirlines() throws IOException {
+
+
+        AirlineParser parser = new AirlineParser(new BufferedReader(new FileReader(filepath)));
+        return parser.makeAirlines();
+    }
 }
---
+++ src/test/java/seng202/group4/ValidatorTest.java
@@ -0,0 +1,8 @@
+package seng202.group4;
+
+/**
+ * Created by jjg64 on 25/08/16.
+ */
+public class ValidatorTest {
+
+}
