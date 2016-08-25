859dd296835044ab072d267f70d0dd5252691a51
Joseph Gardner <jjg64@uclive.ac.nz>
2016-08-25 17:52:37 +1200

    Airline Validator can pick up garbage in a file. Still need to implement no duplicate IDs and whitespace in the same line skipper

--- src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
+++ src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
  * Created by jjg64 on 25/08/16.
  */
 public class AirlineValidator {
-    private final int MAX_ITEMS_PER_LINE = 8;
+    private final int ITEMS_PER_LINE = 8;
     private File filepath;
     private BufferedReader file;
-    private String[] splitLine = new String[MAX_ITEMS_PER_LINE];
+    private String[] splitLine = new String[ITEMS_PER_LINE + 1];
     private String splitBy = ",";
-    private int ID;
-    private boolean active;
     private String currentLine;
+    private boolean isInvalidString;
+    private int lineNumber = 0;
 
     public AirlineValidator(File filepath) throws FileNotFoundException {
         this.filepath = filepath;
@@ -25,9 +25,63 @@ public class AirlineValidator {
     }
 
     public ArrayList<Airline> makeAirlines() throws IOException {
-
+        while ((currentLine = file.readLine()) != null) {
+            lineNumber++;
+            if (!currentLine.matches("\\w")) {
+                validateLine();
+            }
+        }
 
         AirlineParser parser = new AirlineParser(new BufferedReader(new FileReader(filepath)));
         return parser.makeAirlines();
+    }
+
+    private void validateLine() throws IOException {
+        splitLine = currentLine.split(splitBy, ITEMS_PER_LINE + 1);
+        if (splitLine.length != ITEMS_PER_LINE) {
+            System.out.println(splitLine.length);
+            throw new IOException();
+        } else {
+            checkLine();
+        }
+    }
+
+    private void checkLine() throws IOException {
+        try {
+            Integer.parseInt(splitLine[0]);
+        } catch (NumberFormatException e) {
+            System.out.println(0);
+            throw new IOException();
+        }
+
+
+        if (checkString(1)) {   // Name
+            System.out.println(1);
+            throw new IOException();
+        } else if (checkString(2)) {    // Alias
+            System.out.println(2);
+            throw new IOException();
+        } else if (checkString(3) && splitLine[4].length() != 4 && !splitLine[4].equals("\\N")) {  // IATA must be length 2 (inlcudes quotation)
+            System.out.println(3);
+            throw new IOException();
+        } else if (checkString(4) && splitLine[4].length() != 5 && !splitLine[4].equals("\\N")) {  // ICAO must be length 3 (includes quoation)
+            System.out.println(4);
+            throw new IOException();
+        } else if (checkString(5)) {    // Callsign
+            System.out.println(5);
+            throw new IOException();
+        } else if (checkString(6)) {    // Country
+            System.out.println(6);
+            throw new IOException();
+        } else if (!(splitLine[7].toUpperCase().equals("\"Y\"")) && !(splitLine[7].toUpperCase().equals("\"N\""))) {
+            System.out.println(7);
+            throw new IOException();
+        }
+    }
+
+    private boolean checkString(int i) {
+        isInvalidString = !(splitLine[i].equals("\\N"));
+        isInvalidString = isInvalidString && !(splitLine[i].startsWith("\"") && splitLine[i].endsWith("\""));
+        return isInvalidString;
     }
 }
--- src/test/java/seng202/group4/ValidatorTest.java
+++ src/test/java/seng202/group4/ValidatorTest.java
@@ -1,8 +1,67 @@
 package seng202.group4;
 
+import org.junit.Test;
+import seng202.group4.data.dataType.Airline;
+import seng202.group4.data.parser.AirlineParser;
+import seng202.group4.data.parser.validator.AirlineValidator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by jjg64 on 25/08/16.
  */
 public class ValidatorTest {
 
+    /* Airline validator test cases */
+
+    /* Valid files */
+
+    @Test
+    public void oneValidAirlineSize() throws IOException {
+        AirlineValidator parser = new AirlineValidator(new File("testfiles/Airlines/oneValidAirline.txt"));
+        ArrayList<Airline> airlines = parser.makeAirlines();
+        assertTrue(airlines.size() == 1);
+
+    }
+
+    @Test
+    public void multipleValidAirlinesSize() throws IOException {
+        AirlineValidator parser = new AirlineValidator(new File("testfiles/Airlines/validAirline.txt"));
+        ArrayList<Airline> airlines = parser.makeAirlines();
+        assertTrue(airlines.size() == 6048);
+
+    }
+
+    @Test
+    public void oneValidAirlineWithNullSize() throws IOException {
+        AirlineValidator parser = new AirlineValidator(new File("testfiles/Airlines/oneValidAirlineWithNull.txt"));
+        ArrayList<Airline> airlines = parser.makeAirlines();
+        assertTrue(airlines.size() == 1);
+    }
+
+    @Test
+    public void emptyFile() throws IOException {
+        AirlineValidator parser = new AirlineValidator(new File("testfiles/empty.txt"));
+        ArrayList<Airline> airlines = parser.makeAirlines();
+        int size = airlines.size();
+        assertTrue(size == 0);
+    }
+
+    /* Invalid files */
+
+    @Test
+    public void missingQuotation() throws IOException {
+        try {
+            AirlineValidator parser = new AirlineValidator(new File("testfiles/Airlines/oneDodgyAirline.txt"));
+            ArrayList<Airline> airlines = parser.makeAirlines();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
 }
