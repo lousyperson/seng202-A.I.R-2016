7f952cbaf14ece6f0981330f9b622c754df42d64
Pang <psu43@uclive.ac.nz>
2016-08-25 17:09:18 +1200

    added airlineTable to GUI

--- src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
+++ src/main/java/seng202/group4/data/parser/validator/AirlineValidator.java
@@ -2,17 +2,18 @@ package seng202.group4.data.parser.validator;
 import seng202.group4.data.dataType.Airline;
 import seng202.group4.data.parser.AirlineParser;
 import java.io.*;
-import java.util.ArrayList;/**
+import java.util.ArrayList;
+
+/**
  * Created by jjg64 on 25/08/16.
  */
+
 public class AirlineValidator {
     private final int MAX_ITEMS_PER_LINE = 8;
     private File filepath;
     private BufferedReader file;
     private String[] splitLine = new String[MAX_ITEMS_PER_LINE];
     private String splitBy = ",";
-    private int ID;
-    private boolean active;
     private String currentLine;
 
     public AirlineValidator(File filepath) throws FileNotFoundException {
@@ -20,10 +21,8 @@ public class AirlineValidator {
         this.file = new BufferedReader(new FileReader(filepath));
     }
 
-    public ArrayList < Airline > makeAirlines() throws IOException {
-        AirlineParser parser =
-                new AirlineParser(new
-                        BufferedReader(new FileReader(filepath)));
+    public ArrayList<Airline> makeAirlines() throws IOException {
+        AirlineParser parser = new AirlineParser(new BufferedReader(new FileReader(filepath)));
         return parser.makeAirlines();
     }
 
