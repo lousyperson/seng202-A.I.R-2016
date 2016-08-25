0de56ba8576af6ea39d31cff64aa7ceaae40fcc8
Pang <psu43@uclive.ac.nz>
2016-08-25 14:17:53 +1200

    Added controller

--- src/main/java/seng202/group4/GUI/Controller.java
+++ src/main/java/seng202/group4/GUI/Controller.java
@@ -1,4 +1,25 @@
 package seng202.group4.GUI;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import seng202.group4.data.parser.AirlineParser;
+
+import java.io.*;
+
+
 public class Controller {
+
+    public void loadAirline() throws FileNotFoundException {
+
+            Stage stage = new Stage();
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open file");
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                AirlineParser parser = new AirlineParser(br);
+            }
+
+
+    }
 }
