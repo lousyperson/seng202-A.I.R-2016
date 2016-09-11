2493072650af639b5b4f0042c08728388be7bea5
Joseph Gardner <jjg64@uclive.ac.nz>
2016-09-11 15:31:29 +1200

    Flight validator up so it can be used with a table (has no error checking yet)

--- src/main/java/seng202/group4/data/parser/validator/FlightValidator.java
+++ src/main/java/seng202/group4/data/parser/validator/FlightValidator.java
@@ -1,7 +1,25 @@
 package seng202.group4.data.parser.validator;
 
+import seng202.group4.data.dataType.Flight;
+import seng202.group4.data.parser.FlightParser;
+
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Created by jjg64 on 25/08/16.
  */
 public class FlightValidator {
+    private File filepath;
+    private BufferedReader file;
+
+    public FlightValidator(File filepath) throws FileNotFoundException {
+        this.filepath = filepath;
+        this.file = new BufferedReader(new FileReader(filepath));
+    }
+
+    public Flight makeAirports() throws IOException {
+        FlightParser parser = new FlightParser(new BufferedReader(new FileReader(filepath)));
+        return parser.makeFlight();
+    }
 }
