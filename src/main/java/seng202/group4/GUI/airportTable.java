14c15db022bb4e1addf1fbfb701307d6c2dd2d2f
Matt <msi52@uclive.ac.nz>
2016-09-17 16:14:45 +1200

    Added documentation to the flightTable, airportTable, routeTable classes

--- src/main/java/seng202/group4/GUI/airportTable.java
+++ src/main/java/seng202/group4/GUI/airportTable.java
@@ -25,6 +25,21 @@ public class airportTable {
     private SimpleStringProperty atdst;
     private SimpleStringProperty attzdatabase;
 
+    /**
+     * Initializes the variables within the class.
+     * @param ttid
+     * @param ttname
+     * @param ttcity
+     * @param ttcountry
+     * @param ttiata
+     * @param tticao
+     * @param ttlatitude
+     * @param ttlongitude
+     * @param ttaltitude
+     * @param tttimezone
+     * @param ttdst
+     * @param tttzdatabase
+     */
     public airportTable(int ttid, String ttname, String ttcity, String ttcountry, String ttiata, String tticao,
                         double ttlatitude, double ttlongitude, double ttaltitude, float tttimezone, String ttdst, String tttzdatabase){
         this.atid = new SimpleIntegerProperty(ttid);
@@ -45,146 +60,290 @@ public class airportTable {
     }
 
 
+    /**
+     * A getter for the airport id value in the table.
+     * @return
+     */
     public int getAtid() {
         return atid.get();
     }
 
+    /**
+     * Retrieves the id of the airport from the parsed in data.
+     * @return
+     */
     public SimpleIntegerProperty atidProperty() {
         return atid;
     }
 
+    /**
+     * Sets the id value in the airport table
+     * @param atid
+     */
     public void setAtid(int atid) {
         this.atid.set(atid);
     }
 
+    /**
+     * Gets the value of the airport name in the airport table
+     * @return
+     */
     public String getAtname() {
         return atname.get();
     }
 
+    /**
+     * Retrieves the airport name that has been parsed in by the data file.
+     * @return
+     */
     public SimpleStringProperty atnameProperty() {
         return atname;
     }
 
+    /**
+     * Sets the name of the airport in the airport table.
+     * @param atname
+     */
     public void setAtname(String atname) {
         this.atname.set(atname);
     }
 
+    /**
+     * Gets the city of the airport in the airport table.
+     * @return
+     */
     public String getAtcity() {
         return atcity.get();
     }
 
+    /**
+     * Retrieves that value of the airport city as parsed in from the data file.
+     * @return
+     */
     public SimpleStringProperty atcityProperty() {
         return atcity;
     }
 
+    /**
+     * Sets the value of the city in the airport table.
+     * @param atcity
+     */
     public void setAtcity(String atcity) {
         this.atcity.set(atcity);
     }
 
+    /**
+     * Gets the value of the airport country in the airport table.
+     * @return
+     */
     public String getAtcountry() {
         return atcountry.get();
     }
 
+    /**
+     * Retrieves the value of the country from the parsed data.
+     * @return
+     */
     public SimpleStringProperty atcountryProperty() {
         return atcountry;
     }
 
+    /**
+     * Sets the value of the country in the table.
+     * @param atcountry
+     */
     public void setAtcountry(String atcountry) {
         this.atcountry.set(atcountry);
     }
 
+    /**
+     * Gets the airport IATA value for the airport table.
+     * @return
+     */
     public String getAtiata() {
         return atiata.get();
     }
 
+    /**
+     * Retrieves the airport IATA value from the parsed data.
+     * @return
+     */
     public SimpleStringProperty atiataProperty() {
         return atiata;
     }
 
+    /**
+     * Sets the IATA number in the airport table.
+     * @param atiata
+     */
     public void setAtiata(String atiata) {
         this.atiata.set(atiata);
     }
 
+    /**
+     * A getter for teh ICAO number of the airport in the airport table.
+     * @return
+     */
     public String getAticao() {
         return aticao.get();
     }
 
+    /**
+     * Retrieves the airports IATA number from the parsed data.
+     * @return
+     */
     public SimpleStringProperty aticaoProperty() {
         return aticao;
     }
 
+    /**
+     * Sets the airport IATA number in the airport table.
+     * @param aticao
+     */
     public void setAticao(String aticao) {
         this.aticao.set(aticao);
     }
 
+    /**
+     * A getter for the airport latitude in the airport table.
+     * @return
+     */
     public double getAtlatitude() {
         return atlatitude.get();
     }
 
+    /**
+     * Retrieves the airport latitude from the parsed data.
+     * @return
+     */
     public SimpleDoubleProperty atlatitudeProperty() {
         return atlatitude;
     }
 
+    /**
+     * Sets the airport latitude in the airport table.
+     * @param atlatitude
+     */
     public void setAtlatitude(double atlatitude) {
         this.atlatitude.set(atlatitude);
     }
 
+    /**
+     * A getter for the airport longitude in the airport table.
+     * @return
+     */
     public double getAtlongitude() {
         return atlongitude.get();
     }
 
+    /**
+     * Retrieves the longitude of the airport from the parsed data.
+     * @return
+     */
     public SimpleDoubleProperty atlongitudeProperty() {
         return atlongitude;
     }
 
+    /**
+     * Sets the longitude of the airport in the airport table.
+     * @param atlongitude
+     */
     public void setAtlongitude(double atlongitude) {
         this.atlongitude.set(atlongitude);
     }
 
+    /**
+     * A getter for the altitude of the airport in the airport table
+     * @return
+     */
     public double getAtaltitude() {
         return ataltitude.get();
     }
 
+    /**
+     * Retrieves the altitude of the airport from the parsed data.
+     * @return
+     */
     public SimpleDoubleProperty ataltitudeProperty() {
         return ataltitude;
     }
 
+    /**
+     * Sets the altitude of the airport in the airport table.
+     * @param ataltitude
+     */
     public void setAtaltitude(double ataltitude) {
         this.ataltitude.set(ataltitude);
     }
 
+    /**
+     * A getter for the airport timezone in the airport table.
+     * @return
+     */
     public float getAttimezone() {
         return attimezone.get();
     }
 
+    /**
+     * Retrieves the airport timezone form the parsed data.
+     * @return
+     */
     public SimpleFloatProperty attimezoneProperty() {
         return attimezone;
     }
 
+    /**
+     * Sets the airport timezone in the airport table.
+     * @param attimezone
+     */
     public void setAttimezone(float attimezone) {
         this.attimezone.set(attimezone);
     }
 
+    /**
+     * A getter for the airport daylight saving time.
+     * @return
+     */
     public String getAtdst() {
         return atdst.get();
     }
 
+    /**
+     * Retrieves the daylight saving time from the parsed data.
+     * @return
+     */
     public SimpleStringProperty atdstProperty() {
         return atdst;
     }
 
+    /**
+     * Sets the daylight saving time in the airport table.
+     * @param atdst
+     */
     public void setAtdst(String atdst) {
         this.atdst.set(atdst);
     }
 
+    /**
+     * A getter for the airport time zone database for the airport table.
+     * @return
+     */
     public String getAttzdatabase() {
         return attzdatabase.get();
     }
 
+    /**
+     * Retrieves the airport timezone database from the parsed data.
+     * @return
+     */
     public SimpleStringProperty attzdatabaseProperty() {
         return attzdatabase;
     }
 
+    /**
+     * Sets the timezone database in the airport talle.
+     * @param attzdatabase
+     */
     public void setAttzdatabase(String attzdatabase) {
         this.attzdatabase.set(attzdatabase);
     }
--- src/main/java/seng202/group4/GUI/routeTable.java
+++ src/main/java/seng202/group4/GUI/routeTable.java
@@ -4,6 +4,8 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 /**
+ * The routeTable class is called for singular pieces of flight data to enable information that has been parsed in from
+ * a data file to be used in populating a route table.
  * Created by psu43 on 28/08/16.
  */
 public class routeTable {
@@ -31,110 +33,218 @@ public class routeTable {
     }
 
 
+    /**
+     * A getter for the airline name that operates the route for the route table.
+     * @return
+     */
     public String getRairline() {
         return rairline.get();
     }
 
+    /**
+     * Retrieves the name of the airline that operates the route from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rairlineProperty() {
         return rairline;
     }
 
+    /**
+     * Sets the name of the airline that operates the route in the route table.
+     * @param rairline
+     */
     public void setRairline(String rairline) {
         this.rairline.set(rairline);
     }
 
+    /**
+     * A getter for the route id for the route table.
+     * @return
+     */
     public String getRid() {
         return rid.get();
     }
 
+    /**
+     * Retrieves the route id value from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty ridProperty() {
         return rid;
     }
 
+    /**
+     * Sets the route id in the route table.
+     * @param rid
+     */
     public void setRid(String rid) {
         this.rid.set(rid);
     }
 
+    /**
+     * A getter for the souce airport of the route for the route table.
+     * @return
+     */
     public String getRsource() {
         return rsource.get();
     }
 
+    /**
+     * Retrieves the source airport from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rsourceProperty() {
         return rsource;
     }
 
+    /**
+     * Sets the source airport in the route table.
+     * @param rsource
+     */
     public void setRsource(String rsource) {
         this.rsource.set(rsource);
     }
 
+    /**
+     * A getter for the source airport id for the route table.
+     * @return
+     */
     public String getRsourceid() {
         return rsourceid.get();
     }
 
+    /**
+     * Retrieves the source airport id from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rsourceidProperty() {
         return rsourceid;
     }
 
+    /**
+     * Sets the source airport id in the route table.
+     * @param rsourceid
+     */
     public void setRsourceid(String rsourceid) {
         this.rsourceid.set(rsourceid);
     }
 
+    /**
+     * A getter for the destination airport of the route for the route table.
+     * @return
+     */
     public String getRdest() {
         return rdest.get();
     }
 
+    /**
+     * Retrieves the destination airport from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rdestProperty() {
         return rdest;
     }
 
+    /**
+     * Sets the destination airport for the route in the route table.
+     * @param rdest
+     */
     public void setRdest(String rdest) {
         this.rdest.set(rdest);
     }
 
+    /**
+     * A getter for the route's destination airport identification number in the route table.
+     * @return
+     */
     public String getRdestid() {
         return rdestid.get();
     }
 
+    /**
+     * Retrieves the route's destination airport identifaction number from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rdestidProperty() {
         return rdestid;
     }
 
+    /**
+     * Sets the route's destination airport identification number in the route table.
+     * @param rdestid
+     */
     public void setRdestid(String rdestid) {
         this.rdestid.set(rdestid);
     }
 
+    /**
+     * A getter for the route codeshare for the route table.
+     * @return
+     */
     public String getRcodeshare() {
         return rcodeshare.get();
     }
 
+    /**
+     *Retrieves the codeshare value for the route from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rcodeshareProperty() {
         return rcodeshare;
     }
 
+    /**
+     * Sets the codeshare value for the route in the route table.
+     * @param rcodeshare
+     */
     public void setRcodeshare(String rcodeshare) {
         this.rcodeshare.set(rcodeshare);
     }
 
+    /**
+     * A getter for the routes number of stops for the route table.
+     * @return
+     */
     public String getRstops() {
         return rstops.get();
     }
 
+    /**
+     * Retrieves the number of stops for the route from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty rstopsProperty() {
         return rstops;
     }
 
+    /**
+     * Sets the routes number of stops in the route table.
+     * @param rstops
+     */
     public void setRstops(String rstops) {
         this.rstops.set(rstops);
     }
 
+    /**
+     * A getter for the equipment utilised by the route for the route table.
+     * @return
+     */
     public String getRequipment() {
         return requipment.get();
     }
 
+    /**
+     * Retrieves the equipment information from the parsed route data.
+     * @return
+     */
     public SimpleStringProperty requipmentProperty() {
         return requipment;
     }
 
+    /**
+     * Sets the route equipment information in the route table.
+     * @param requipment
+     */
     public void setRequipment(String requipment) {
         this.requipment.set(requipment);
     }
